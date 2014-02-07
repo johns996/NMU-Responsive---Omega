acd0e64c8d99933f1a1daddf531083ef6c6d872c
johns996 <eric.johnson.x@gmail.com>
2014-02-07 14:35:24 -0500

    attempting to game the google thumbnail picking the second image in the body area

--- templates/node.tpl.php
+++ templates/node.tpl.php
@@ -12,16 +12,18 @@
   <footer class="submitted"><?php print $date; ?> -- <?php print $name; ?></footer>
   <?php endif; ?>
   */ ?>
-  
+
   <div<?php print $content_attributes; ?>>
     <?php
       // We hide the comments and links now so that we can render them later.
       hide($content['comments']);
       hide($content['links']);
+      //add the nmu logo again at the start of the content area.  hopefully this tricks google into using the first image on a page as the thumbnail instead of the second
+      print '<img src="/sites/all/themes/omega_nmu/images/nmu_logo_white.png" style="width:0px; height:0px; margin:0; padding:0; float:left;" />';
       print render($content);
     ?>
   </div>
-  
+
   <div class="clearfix">
     <?php if (!empty($content['links'])): ?>
       <nav class="links node-links clearfix"><?php print render($content['links']); ?></nav>
