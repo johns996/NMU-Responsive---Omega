dc97abcd8ea6a8d79d543cc20b147c7fe249aced
johns996 <eric.johnson.x@gmail.com>
2014-04-03 11:55:44 -0400

    not going to deal with symlinks

--- nmu_omega_theme_includes/nmu_footer_first.php
+++
@@ -1,5 +0,0 @@
-<div id="footer_region_first">
-	<a href="/" title="NMU Homepage">
-		<img border="0" width="151" height="57" id="footer_logo" longdesc="/communicationsandmarketing/node/38" alt="Northern Michigan University - Northern. Naturally." src="/sites/all/themes/omega_nmu/images/nmu_logo_white.png" />
-	</a>
-</div><!-- /#footer_region_first -->
--- nmu_omega_theme_includes/nmu_footer_second.php
+++
@@ -1,5 +0,0 @@
-<div id="footer_region_second">
-		<p>1401 Presque Isle Ave. <span class="mobile_hide">&bull;</span> <span class="mobile_break"></span>Marquette, MI 49855-5301 <span class="mobile_hide">&bull;</span> <span class="mobile_break"></span>906-227-1000<span class="mobile_break"></span><br />
-		<a href="/copyright">&copy;</a> <?php echo date('Y') ?> by the NMU <a href="/board">Board of Trustees</a>. <span class="mobile_break"></span>NMU is an <a href="/aaeo">equal opportunity</a> institution.<span class="mobile_break"></span><br />
-		<span class="print_hide"><a href="mailto:admiss@nmu.edu">Admissions Questions</a> | <a href="/contactus">Contact Us</a> <span class="mobile_hide">|</span> <span class="mobile_break"></span><a href="/communicationsandmarketing/PrivacyStatement">Privacy Statement</a> | <a href="/emergency">Emergency Information</a></span></p>
-</div><!-- /#footer_region_second -->
