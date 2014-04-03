5041edff5290b20f6ab3303dcb731575b6449533
johns996 <eric.johnson.x@gmail.com>
2014-04-03 09:42:18 -0400

    added 1.0 versions of hp-v2 files

---
+++ js/homepage-v2.js
@@ -0,0 +1,36 @@
+/*!
+ * homepage v2 javascripts
+ */
+
+var slideshowWrapper = '\
+<div class="cycle-slideshow"\
+     data-cycle-timeout="0"\
+     data-cycle-caption="#custom-caption"\
+     data-cycle-caption-template="<h2>{{alt}}</h2><p>{{cycleTitle}}</p>">\
+    <div id="custom-caption"></div>\
+    <div class="cycle-pager"></div>\
+</div>';
+
+jQuery(document).ready(function($) {
+    createSlideshow();
+
+    function createSlideshow() {
+	// add cycle2
+	$("head").append('<script src="//malsup.github.com/jquery.cycle2.js"></script>\
+	    <script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>');
+	$("#node_newhome_full_group_main_wrapper").hide();
+	//$("#node_newhome_full_group_sub_wrapper1").hide();
+	//$("#node_newhome_full_group_sub_wrapper2").hide();
+	//$("#node_newhome_full_group_sub_wrapper3").hide();
+	$("#node_newhome_full_group_main_wrapper").before(slideshowWrapper);
+	var images = $("#node_newhome_full_group_main_wrapper img");
+	var titles = $(".field-name-field-main-title .field-item");
+	var text = $(".field-name-field-main-text .field-item");
+	for( var i = 0; i < images.size(); i++ ) {
+		images.get(i).setAttribute('alt', titles.get(i).innerHTML);
+		images.get(i).setAttribute('data-cycle-title', text.get(i).innerHTML);
+	}
+	images.appendTo('.cycle-slideshow');
+
+    }
+});
