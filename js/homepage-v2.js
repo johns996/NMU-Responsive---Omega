c6cd715837e6f52ef43181f959c168171b8a42e3
johns996 <eric.johnson.x@gmail.com>
2014-04-11 17:05:26 -0400

    fixed main image bouncing around on page load with a hide and show call

--- js/homepage-v2.js
+++ js/homepage-v2.js
@@ -13,23 +13,25 @@ var slideshowWrapper = '\
 </div>';
 
 jQuery(document).ready(function($) {
-    createSlideshow();
+	//debugger;
 
-    function createSlideshow() {
-	// add cycle2
-	$("head").append('<script src="//malsup.github.com/jquery.cycle2.js"></script>\
-			<script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>\
-			<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>');
-	$("#node_newhome_full_group_main_wrapper").hide();
-	$("#node_newhome_full_group_main_wrapper").before(slideshowWrapper);
-	var images = $("#node_newhome_full_group_main_wrapper img");
-	var titles = $(".field-name-field-main-title .field-item");
-	var text = $(".field-name-field-main-text .field-item");
-	for( var i = 0; i < images.size(); i++ ) {
-		images.get(i).setAttribute('alt', titles.get(i).innerHTML);
-		images.get(i).setAttribute('data-cycle-title', text.get(i).innerHTML);
-	}
-	images.appendTo('.cycle-slideshow');
+   createSlideshow();
 
-    }
+   function createSlideshow() {
+		$("head").append('<script src="//malsup.github.com/jquery.cycle2.js"></script>\
+				<script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>\
+				<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>');
+		$("#node_newhome_full_group_main_wrapper").hide();
+		$("#node_newhome_full_group_main_wrapper").before(slideshowWrapper);
+		$('#custom-caption, .cycle-pager').hide();  //hide these elements so the main image does not bounce around on load
+		var images = $("#node_newhome_full_group_main_wrapper img");
+		var titles = $(".field-name-field-main-title .field-item");
+		var text = $(".field-name-field-main-text .field-item");
+		for( var i = 0; i < images.size(); i++ ) {
+			images.get(i).setAttribute('alt', titles.get(i).innerHTML);
+			images.get(i).setAttribute('data-cycle-title', text.get(i).innerHTML);
+		}
+		images.appendTo('.cycle-slideshow');
+		$('.cycle-pager').show();  //cycle2 takes care of showing the caption, only need to display the pager again
+   }
 });
